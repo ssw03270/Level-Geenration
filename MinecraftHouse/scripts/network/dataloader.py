98a4ebb382ff75cb8ca71ded4da565ad6853fa5f
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-01-22 17:37:24 +0900

    [Fix] fix model data

--- MinecraftHouse/scripts/network/dataloader.py
+++ MinecraftHouse/scripts/network/dataloader.py
@@ -99,7 +99,7 @@ class CraftAssistDataset(Dataset):
             pad_mask_sequence = [1] * (2048 - pad_length) + [0] * pad_length
 
             self.text_sequences.append(text_sequence)
-            print(position_sequence)
+            print(len(position_sequence))
             self.position_sequences.append(position_sequence)
             self.id_sequences.append(id_sequence)
             self.category_sequences.append(category_sequence)
