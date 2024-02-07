20445d0afd04839d5643fde3ce8e9af66464dd3d
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-02-07 14:24:56 +0900

    [Fix] fix model

--- MinecraftHouse/scripts/network/graph/model.py
+++ MinecraftHouse/scripts/network/graph/model.py
@@ -20,6 +20,7 @@ class ScaledDotProductAttention(nn.Module):
         attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
 
         if mask is not None:
+            print(q.shape, attn.shape, mask.shape)
             attn = attn.masked_fill(mask == 0, -1e9)
 
         attn = F.softmax(attn, dim=-1)
