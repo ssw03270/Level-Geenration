bc7e6e6979cbcde9929a066f79a46be9d8f8f731
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-02-14 23:01:43 +0900

    [Fix] del gcn

--- MinecraftHouse/scripts/network/graph/model.py
+++ MinecraftHouse/scripts/network/graph/model.py
@@ -207,8 +207,11 @@ class GraphEncoder(nn.Module):
         # latent = self.aggregate(g_embed)
 
         # return latent, attn
-        g_embed_t = self.global_pool(n_embed_t, data.batch)
-        return g_embed_t, attn
+        batch_index = data.batch  # 각 노드가 어느 배치에 속하는지의 인덱스
+        batch_size = batch_index.max().item() + 1  # 배치 크기 (그래프의 수)
+        first_node_indices =  batch_index.sort()[1][batch_index.sort()[1] == torch.arange(batch_size).unsqueeze(1)].min(dim=1)[0]
+
+        return n_embed_t[first_node_indices], attn
 class GenerativeModel(nn.Module):
     def __init__(self, n_layer, d_model):
         super(GenerativeModel, self).__init__()
